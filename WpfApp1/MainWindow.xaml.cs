efb9ebf2183ca40682c943b2fffb17929771466b
vladi40k <tymoshchuk.if@gmail.com>
2020-03-20 22:03:43 +0200

    first commit

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SerialPort _serialPort;
+        public MainWindow(){
+            StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+
+            _serialPort = new SerialPort();
+
+            InitializeComponent();
+
+            Button_Start.IsEnabled = false;
+            Button_Stop.IsEnabled = false;
+
+            InitCOMsList();
+
+            _serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+        }
+
+
+
+        public void InitCOMsList() {
+            ComboBox_COMs.Items.Clear();
+            foreach (string COM in SerialPort.GetPortNames())
+                ComboBox_COMs.Items.Add(COM);
+        }
+        private void Button_Reflesh_COMs_list_Click(object sender, RoutedEventArgs e){
+            InitCOMsList();
+            Button_Start.IsEnabled = false;
+        }
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e){
+            SerialPort sp = (SerialPort)sender;
+            this.Dispatcher.Invoke(() =>
+            {
+                ListBox_Chat.Items.Add(sp.ReadExisting());
+                var border = (Border)VisualTreeHelper.GetChild(ListBox_Chat, 0);
+                var scrollViewer = (ScrollViewer)VisualTreeHelper.GetChild(border, 0);
+                scrollViewer.ScrollToBottom();
+            });
+        }
+        private void Button_Start_Click(object sender, RoutedEventArgs e){
+            
+            Button_Stop.IsEnabled = true;
+            Button_Start.IsEnabled = false;
+            try
+            {
+                _serialPort.PortName = ComboBox_COMs.SelectedItem.ToString();
+                _serialPort.BaudRate = int.Parse(ComboBox_Bundrate.Text);
+                _serialPort.Open();
+            }
+            catch (Exception ex) {
+                Button_Stop.IsEnabled = false;
+                Button_Start.IsEnabled = false;
+                MessageBox.Show("Some setings is wrong!", "Error" , MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+
+        }
+        private void Button_Stop_Click(object sender, RoutedEventArgs e) {
+            Button_Stop.IsEnabled = false;
+            Button_Start.IsEnabled = true;
+
+            _serialPort.Close();
+        }
+        private void ComboBox_COMs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(ComboBox_Bundrate.SelectedItem != null)
+                Button_Start.IsEnabled = true;
+        }
+        private void ComboBox_Bundrate_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBox_COMs.SelectedItem != null)
+                Button_Start.IsEnabled = true;
+        }
+    }
+}
+
+public class PortChat
+{
+    static bool _continue;
+    static SerialPort _serialPort;
+
+    public static void XMain()
+    {
+
+        StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+        Thread readThread = new Thread(Read);
+
+        // Create a new SerialPort object with default settings.
+        _serialPort = new SerialPort();
+
+        // Allow the user to set the appropriate properties.
+        _serialPort.PortName = SetPortName(_serialPort.PortName);
+        _serialPort.BaudRate = SetPortBaudRate(_serialPort.BaudRate);
+
+        // Set the read/write timeouts
+        _serialPort.ReadTimeout = 500;
+        _serialPort.WriteTimeout = 500;
+
+        _serialPort.Open();
+        _continue = true;
+        readThread.Start();
+
+        Console.WriteLine("Type QUIT to exit");
+
+        String message;
+        while (_continue)
+        {
+            message = Console.ReadLine();
+
+            if (stringComparer.Equals("quit", message))
+            {
+                _continue = false;
+            }
+            else
+            {
+                _serialPort.WriteLine(
+                    String.Format("COM: {0}", message));
+            }
+        }
+
+        readThread.Join();
+        _serialPort.Close();
+    }
+
+    public static void Read()
+    {
+        while (_continue)
+        {
+            try
+            {
+                string message = _serialPort.ReadLine();
+                Console.WriteLine(message);
+            }
+            catch (TimeoutException) { }
+        }
+    }
+
+    // Display Port values and prompt user to enter a port.
+    public static string SetPortName(string defaultPortName)
+    {
+        string portName;
+
+        Console.WriteLine("Available Ports:");
+        foreach (string s in SerialPort.GetPortNames())
+        {
+            Console.WriteLine("   {0}", s);
+        }
+
+        Console.Write("Enter COM port value (Default: {0}): ", defaultPortName);
+        portName = Console.ReadLine();
+
+        if (portName == "" || !(portName.ToLower()).StartsWith("com"))
+        {
+            portName = defaultPortName;
+        }
+        return portName;
+    }
+    // Display BaudRate values and prompt user to enter a value.
+    public static int SetPortBaudRate(int defaultPortBaudRate)
+    {
+        string baudRate;
+
+        Console.Write("Baud Rate(default:{0}): ", defaultPortBaudRate);
+        baudRate = Console.ReadLine();
+
+        if (baudRate == "")
+        {
+            baudRate = defaultPortBaudRate.ToString();
+        }
+
+        return int.Parse(baudRate);
+    }
+}
